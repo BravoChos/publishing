b78ee734b59262b29c85e5efd759f30576cb7882
Song Cho <iamsongcho@gmail.com>
2022-06-22 00:19:00 +0900

    Converting our code to OOP

--- freedomCorp/js/main.js
+++ freedomCorp/js/main.js
@@ -1,16 +1,22 @@
 /*
 *    main.js
 *    Mastering Data Visualization with D3.js
-*    10.2 - File Separation
+*    10.4 - Converting our code to OOP
 */
+
+let lineChart1
+let lineChart2
+let lineChart3
+let lineChart4
+let lineChart5
 
 // time parsers/formatters
 const parseTime = d3.timeParse("%d/%m/%Y")
 const formatTime = d3.timeFormat("%d/%m/%Y")
 
 // event listeners
-$("#coin-select").on("change", update)
-$("#var-select").on("change", update)
+$("#coin-select").on("change", updateCharts)
+$("#var-select").on("change", updateCharts)
 
 // add jQuery UI slider
 $("#date-slider").slider({
@@ -25,7 +31,7 @@ $("#date-slider").slider({
 	slide: (event, ui) => {
 		$("#dateLabel1").text(formatTime(new Date(ui.values[0])))
 		$("#dateLabel2").text(formatTime(new Date(ui.values[1])))
-		update()
+		updateCharts()
 	}
 })
 
@@ -45,7 +51,17 @@ d3.json("data/coins.json").then(data => {
 			})
 	})
 
-	// run the visualization for the first time
-	update()
+	lineChart1 = new LineChart("#chart-area1", "bitcoin")
+	lineChart2 = new LineChart("#chart-area2", "ethereum")
+	lineChart3 = new LineChart("#chart-area3", "bitcoin_cash")
+	lineChart4 = new LineChart("#chart-area4", "litecoin")
+	lineChart5 = new LineChart("#chart-area5", "ripple")
 })
 
+function updateCharts(){
+	lineChart1.wrangleData()
+	lineChart2.wrangleData()
+	lineChart3.wrangleData()
+	lineChart4.wrangleData()
+	lineChart5.wrangleData()
+}
